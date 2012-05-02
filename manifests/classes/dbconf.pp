522b8849bfc84a9b4e1cdfa1412d2f356acfdaff
Dave Simons <dave@inuits.eu>
2012-05-02 15:28:45 +0200

    replace old apache::apache vars with apache::params::service_name
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/classes/dbconf.pp
+++ manifests/classes/dbconf.pp
@@ -7,7 +7,7 @@ class redmine::dbconf {
 			provider => shell,
 			command => 'rake db:migrate',
 			require => Mysql_db[$redmine::production_db],
-			notify => Service["$apache::apache"];
+			notify => Service["$apache::params::service_name"];
 		'load_default_data':
 			environment => ['RAILS_ENV=production', 'REDMINE_LANG=en'],
 			path => "$ruby::bin_dir",
@@ -15,6 +15,6 @@ class redmine::dbconf {
 			provider => 'shell',
 			command => 'rake redmine:load_default_data',
 			require => [ Mysql_db[$redmine::production_db], Exec['config_redmine_mysql_bootstrap'] ],
-			notify => Service["$apache::apache"];
+			notify => Service["$apache::params::service_name"];
 	}
 }
