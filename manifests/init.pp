6fe13f559b7ba993a526e17bb084cd1700fc4d61
Jan Vansteenkiste <jan@vstone.eu>
2012-07-31 11:02:43 +0200

    Cleaner class dependencies and retab in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,34 @@
 class redmine (
-	$production_db = 'redmine_production',
-	$devel_db = 'redmine_devel',
-	$dbuser = 'redmine',
-	$dbpass = 'redmine',
-	$dbhost = 'localhost',
-	$dbserver = 'localhost',
-	$home = '/usr/share/redmine',
-	$plugins = [],
-	$mail_user = 'redmine',
-	$mail_pass = 'redmine',
-	$mail_auth = 'plain',
-	$mail_domain = 'redmine.org',
-	$mail_port = '587',
-	$mail_smtp = 'smtp.redmine.org',
-	$mail_tls = 'true',
-	$themes = []
+  $production_db = 'redmine_production',
+  $devel_db = 'redmine_devel',
+  $dbuser = 'redmine',
+  $dbpass = 'redmine',
+  $dbhost = 'localhost',
+  $dbserver = 'localhost',
+  $home = '/usr/share/redmine',
+  $plugins = [],
+  $mail_user = 'redmine',
+  $mail_pass = 'redmine',
+  $mail_auth = 'plain',
+  $mail_domain = 'redmine.org',
+  $mail_port = '587',
+  $mail_smtp = 'smtp.redmine.org',
+  $mail_tls = 'true',
+  $themes = []
 ) {
-	if ! defined(Class['::repos']) { include ::repos }
+  if ! defined(Class['::repos']) { include ::repos }
 
-	class {
-		'redmine::pre':
-			before => Class['redmine::depends'];
-		'redmine::depends':
-			before => Class['redmine::config'];
-		'redmine::config':
-			before => Class['redmine::dbconf'];
-		'redmine::dbconf':
-			require => Service['mysqld'],
-			before => Class['redmine::plugins'];
-		'redmine::plugins':
-			before => Class['redmine::themes'];
-		'redmine::themes':;
-	}
+  class {'::redmine::pre': } ->
+  class {'::redmine::depends': } ->
+  class {'::redmine::config': } ->
+  class {'::redmine::dbconf':
+      require => Service['mysqld'],
+  } ->
+  class {'::redmine::plugins': } ->
+  class {'::redmine::themes': }
 
-    Exec {
-		path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'
-	}
+  Exec {
+    path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'
+  }
+
 }
