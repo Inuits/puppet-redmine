075538b79e82922f8edfa660c58a0b2a9bd27e5f
Jan Vansteenkiste <jan@vstone.eu>
2012-07-31 11:04:08 +0200

    Retab ALL the files!
    Conflicts:
    	manifests/depends.pp

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -1,10 +1,10 @@
 define redmine::theme (
-	$url
+  $url
 ) {
-	exec {
-		"install_theme_$name":
-			command => "git clone $url $name",
-			creates => "$redmine::home/public/themes/$name",
-			cwd => "$redmine::home/public/themes";
-	}
+  exec {
+    "install_theme_$name":
+      command => "git clone $url $name",
+      creates => "$redmine::home/public/themes/$name",
+      cwd => "$redmine::home/public/themes";
+  }
 }
--- manifests/themes.pp
+++ manifests/themes.pp
@@ -1,25 +1,25 @@
 class redmine::themes {
-	@redmine::theme {
-		'pepper':
-			url => 'https://github.com/koppen/redmine-pepper-theme.git';
+  @redmine::theme {
+    'pepper':
+      url => 'https://github.com/koppen/redmine-pepper-theme.git';
 
-		'aurora':
-			url => 'https://github.com/jorgebg/redmine-aurora-theme.git';
+    'aurora':
+      url => 'https://github.com/jorgebg/redmine-aurora-theme.git';
 
-		'innerboard':
-			url => 'https://github.com/huacnlee/redmine-theme-innerboard.git';
+    'innerboard':
+      url => 'https://github.com/huacnlee/redmine-theme-innerboard.git';
 
-		'pixelcookers':
-			url => 'https://github.com/pixel-cookers/RedmineThemePixelCookers.git';
+    'pixelcookers':
+      url => 'https://github.com/pixel-cookers/RedmineThemePixelCookers.git';
 
-		'squeejee':
-			url => 'https://github.com/edavis10/squeejee_theme.git';
+    'squeejee':
+      url => 'https://github.com/edavis10/squeejee_theme.git';
 
-		'martini':
-			url => 'https://github.com/bevanr/martini.git';
-	}
+    'martini':
+      url => 'https://github.com/bevanr/martini.git';
+  }
 
-	if $redmine::themes {
-		realize(Redmine::Theme[$redmine::themes])
-	}
+  if $redmine::themes {
+    realize(Redmine::Theme[$redmine::themes])
+  }
 }
